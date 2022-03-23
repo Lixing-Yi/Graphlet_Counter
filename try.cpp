ffac8564dad7546b6a6672e62a9005e8f3579636
Lixing Yi <yilixing233@gmail.com>
2022-03-22 20:03:46 -0700

    add permutation datas

--- try.cpp
+++ try.cpp
@@ -1,10 +1,13 @@
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
 int main()
 {
-    vector<int> v1{1,2,3};
-    vector<int> v2{4,5,6};
-    vector<vector<int>> v{v1,v2};
+    vector <int> v1{1,2,3};
+    vector <int> v2{4,5,6};
+    vector <vector <int>> v{v1,v2};
     cout <<v1[1];
     return 0;
 }
